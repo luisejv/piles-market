93c6a5617a2c60d6bda9efecea9c3dd20064ad2a
Juan Galvez <juangalvezccopa@gmail.com>
2021-11-23 19:15:38 -0500

    Change font and tittle color

--- components/elements/basic/Logo.js
+++ components/elements/basic/Logo.js
@@ -2,23 +2,19 @@ import React from 'react';
 import Link from 'next/link';
 
 const Logo = ({ url = '/', light }) => {
-    if (light) {
-        return (
-            <Link href={url}>
-                <a className="ps-logo">
-                    <img src="/static/img/logo-white.png" alt="" />
-                </a>
-            </Link>
-        );
-    } else {
-        return (
-            <Link href={url}>
-                <a className="ps-logo">
-                    <img src="/static/img/logo.svg" alt="" />
-                </a>
-            </Link>
-        );
-    }
+  if (light) {
+    return (
+      <Link href={url}>
+        <a className="ps-logo">PILES</a>
+      </Link>
+    );
+  } else {
+    return (
+      <Link href={url}>
+        <a className="ps-logo">PILES</a>
+      </Link>
+    );
+  }
 };
 
 export default Logo;
